0bbffcf8da0223def36ca5b65f1cff086584efb4
Saimom <saimomislam16@gmail.com>
2021-06-21 21:24:51 +0600

    3rd commit

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -7,20 +7,24 @@ import About from './About.js';
 import Contact from './Contact.js';
 import Service from './Service.js'
 import Navber from './Navbar.js';
+import Footer from './Footer.js';
 import { Switch, Route, Redirect, Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div >
-      <Navber />
+    <div className="page-container">
+      <div className="wrap-content">
+        <Navber />
 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/About" component={About} />
-        <Route exact path="/Contact" component={Contact} />
-        <Route exact path="/Service" component={Service} />
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/Service" component={Service} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+      <Footer />
 
 
     </div>
---
+++ e-commerce/src/Footer.js
@@ -0,0 +1,19 @@
+import { React } from 'react'
+
+
+const Footer = () => {
+    return (
+
+
+        <div>
+            <footer className="text-center bg-light w-100  ">
+                <p className="my-3"><i className="fa fa-copyright" style={{ fontsize: '24px' }}></i>2020|All Rights Reserved To |LearnMore</p>
+            </footer>
+
+
+
+        </div>
+    )
+}
+
+export default Footer;
